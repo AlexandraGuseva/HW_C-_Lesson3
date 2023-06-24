80962ff9c32cc723275732aedfa46925838c4101
sasha <voropaeva-sasha@mail.ru>
2023-06-24 13:53:50 +0300

    Changed Tasks

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -16,9 +16,7 @@ int Coordinate(string coorName, string pointName)
     return Convert.ToInt32(Console.ReadLine());
 }
 
-double Decision(double x1, double x2,
-                double y1, double y2,
-                double z1, double z2)
+double Decision(double x1, double x2, double y1, double y2, double z1, double z2)
 {
     return Math.Sqrt(Math.Pow((x2 - x1), 2) +
                      Math.Pow((y2 - y1), 2) +
